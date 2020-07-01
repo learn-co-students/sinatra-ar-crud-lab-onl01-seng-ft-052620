ef8cfcc61f8273ce5aeb006a5524d8ec33ab862d
dwandrew <danwandrew@gmail.com>
2020-07-01 13:47:40 +0100

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,48 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to "/articles"
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @article =Article.create(title: params["title"], content: params["content"])
+    # erb :show
+    redirect to "/articles/#{Article.last.id}"
+  end
+
+  get '/articles/:id' do
+    if params["id"].to_i <= Article.last.id
+    @article = Article.find(params["id"])
+    erb :show
+    else
+      "Sorry no Article of that index"
+    end
+  end
+
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+  delete "/articles/:id" do
+    @article = Article.find(params["id"])
+    @article.delete
+    redirect to '/articles'
+  end
+  
 end
