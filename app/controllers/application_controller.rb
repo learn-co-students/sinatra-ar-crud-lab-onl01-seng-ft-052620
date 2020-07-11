efb4ab0d50f5f65c1484709f371580c3fcf86a79
Eyeruz <3yerus@gmail.com>
2020-07-11 18:01:48 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,35 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+  
   end
+
+  get '/articles' do
+@articles = Article.all 
+    erb :index
+  end
+
+  get '/articles/:id' do 
+
+@article = params[:id]
+
+    erb :show
+  end
+
+  get '/articles/new' do 
+
+      erb :new
+      binding.pry
+      
+  end
+
+  post '/articles' do 
+
+ @article = Article.create(params)
+
+redirect to "/articles/#{ @article.id }"
+
+  erb :show
+end
+
 end
