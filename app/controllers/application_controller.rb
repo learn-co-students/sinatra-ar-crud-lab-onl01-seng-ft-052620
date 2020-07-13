ac381b464f7fa87d8da80fc7d29183f7a4f759b1
JohnAjaka <johnajaka96@gmail.com>
2020-07-13 18:26:55 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,22 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end 
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do 
+    @article = Article.new(params[:article])
+  end
 end
